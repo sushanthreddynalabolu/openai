d2458bca25bb6a6d3272873b769c26fb3ecdfa7b
sushanthreddy <sushanthreddynalabolu@gmail.com>
2023-01-01 18:22:00 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -61,7 +61,7 @@ const handleSubmit=async(e)=>{
   const messageDiv=document.getElementById(uniqueId);
   loader(messageDiv)
   //fetch the response or data from server
-  const response= await fetch('http://localhost:5000',{
+  const response= await fetch('https://openai-qvoc.onrender.com',{
     method:'POST',
     headers:{
       'Content-Type':'application/json'
